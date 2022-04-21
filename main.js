73b0f997103a26a06f52c16694d0ce05b29ef8c2
Amanda Sands <amandajean555@gmail.com>
2022-04-21 08:55:55 -0600

    Add query selectors for gameboard

---
+++ main.js
@@ -0,0 +1,19 @@
+//Query Selectors
+var square1 = document.querySelector("#square1");
+var square2 = document.querySelector("#square2");
+var square3 = document.querySelector("#square3");
+var square4 = document.querySelector("#square4");
+var square5 = document.querySelector("#square5");
+var square6 = document.querySelector("#square6");
+var square7 = document.querySelector("#square7");
+var square8 = document.querySelector("#square8");
+var square9 = document.querySelector("#square9");
+
+//Event Listeners
+square1.addEventListener("click", sayHowdy)
+
+
+//Event Handlers
+sayHowdy() {
+  console.log('Howdy!')
+}
